13f225dc40076c504e5f7f034a7e4e83ec20dd15
danielmast <danielmast92@gmail.com>
2020-10-10 14:07:44 +0200

    Add initialized Spring Boot project

---
+++ src/main/java/nl/danielmast/sourcelabs/SourcelabsApplication.java
@@ -0,0 +1,13 @@
+package nl.danielmast.sourcelabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourcelabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourcelabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/danielmast/sourcelabs/SourcelabsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.danielmast.sourcelabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourcelabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
